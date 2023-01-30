24a884d86958e8468df1ff8087a1863ab540a592
Miguel Caçador Peixoto <miguelpeixoto457@gmail.com>
2023-01-30 16:53:36 +0000

    working app

--- app.py
+++ app.py
@@ -6,118 +6,65 @@ import numpy as np
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 from tqdm import tqdm
+import gc
 
 
-use_gpu = False
-device = 'cuda' if use_gpu else 'cpu'
 
-
-@st.cache
+#@st.cache(persist=True, max_entries=1)
+@st.experimental_singleton
 def load_data():
+    print("Loading data!")
     data = pd.read_csv('data/arxiv_processed.csv')
-    data['text'] = data['title'] + " " + data['abstract']
+    # Prune abstract to 50 characters
+    data['abstract'] = data['abstract'].apply(lambda x: x[:50] + '...' if len(x) > 50 else x)
+    #data['text'] = data['title'] + " " + data['abstract']
     return data
 
 
 
+#@st.cache(persist=True, max_entries=1)
+@st.experimental_singleton
 
-# Load the index
-class FaissIdx:
-    def __init__(self, model, dim=768, batch_size=64):
-        # Maintaining the document data
-        # self.doc_map = dict()
-        self.model = model
-        self.batch_size = batch_size
-
-        # Initialize the index
-        self.index = faiss.IndexFlatIP(dim)
-
-        # Use GPU
-        if use_gpu:
-            res = faiss.StandardGpuResources()
-            self.index = faiss.index_cpu_to_gpu(res, 0, self.index)
-
-        # Counter for number of documents
-        self.ctr = 0
-
-    def add_doc(self, document_text):
-        save_every = 10
-
-        if self.ctr >= len(document_text):
-            print("No more documents to add!")
-            return
-
-        for i in tqdm(range(self.ctr, len(document_text), self.batch_size), desc="Adding documents to index", unit="batch"):
-            self.index.add(self.model.encode(document_text[i:i+self.batch_size]))
-
-            if i % save_every == 0:
-                self.save_index('index.faiss')
-
-        #self.doc_map[self.ctr] = document_text # store the original document text
-
-        #def search_doc(self, query, k=3):
-        #    D, I = self.index.search(self.model.encode(query).reshape(1, -1), k)
-        #    return [{self.doc_map[idx]: score} for idx, score in zip(I[0], D[0]) if idx in self.doc_map]
-
-    def load_index(self, index_path):
-        print("Loading index!")
-        # Convert index to cpu
-        if use_gpu:
-            self.index = faiss.index_gpu_to_cpu(self.index)
-
-        self.index = faiss.read_index(index_path)
-        self.ctr = self.index.ntotal
-
-        if use_gpu:
-            # Convert index back to gpu
-            res = faiss.StandardGpuResources()
-            self.index = faiss.index_cpu_to_gpu(res, 0, self.index)
+def load_model():
+    print("Loading model!")
+    model = SentenceTransformer('all-mpnet-base-v2', device='cuda')
+    return model
 
 
-
-    def save_index(self, index_path):
-        if use_gpu:
-            # Convert index to cpu
-            self.index = faiss.index_gpu_to_cpu(self.index)
-
-        faiss.write_index(self.index, index_path)
-
-        # Convert index back to gpu
-        if use_gpu:
-            res = faiss.StandardGpuResources()
-            self.index = faiss.index_cpu_to_gpu(res, 0, self.index)
-
-
-@st.cache
+#@st.cache(persist=True, max_entries=1)
+@st.experimental_singleton
 def load_index():
-    model = SentenceTransformer('all-mpnet-base-v2', device=device)
-    index = FaissIdx(model)
-    index.load_index('index.faiss')
+    print("Loading index!")
+    index = faiss.read_index('index.faiss')
+
+
     return index
 
 
-if 'data' not in st.session_state:
-    st.session_state.data = load_data()
-    st.session_state.index = load_index()
+
+model = load_model()
+data = load_data()
+index = load_index()
+
 
 def main():
     st.title("Arxiv Search Engine")
     query = st.text_input("Search query")
     k = st.slider("Number of results", 1, 10, 3)
 
-    index = st.session_state.index
-    data = st.session_state.data
-
 
     if query:
-        D, I = index.index.search(index.model.encode(query).reshape(1, -1), k)
+        D, I = index.search(model.encode(query).reshape(1, -1), k)
 
         df = pd.DataFrame()
         for idx, score in zip(I[0], D[0]):
-            df = df.append(data.iloc[idx])
+            df = pd.concat([df, data.iloc[idx:idx+1]])
 
-        st.write(df[['title', 'abstract', 'url']])
-        st.balloons()
+        st.table(df[['title', 'abstract', 'url']])
+        #st.balloons()
+        del df
+        gc.collect()
+
         
 
 if __name__ == "__main__":
