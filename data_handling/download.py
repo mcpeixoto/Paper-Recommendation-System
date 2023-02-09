1e7190c8377dd97701d0654682c42f4ec0b69e7c
Miguel Caçador Peixoto <miguelpeixoto457@gmail.com>
2023-02-09 00:48:42 +0000

    concatenated data_handling to download.py

--- data_handling/download.py
+++ data_handling/download.py
@@ -7,6 +7,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.
 import os
 from os.path import join, expanduser, exists
 from config import data_dir
+from tqdm import tqdm
+import pandas as pd
 
 # Check if the user has the kaggle.json file
 if not exists(join(expanduser("~"), ".kaggle", "kaggle.json")):
@@ -36,6 +38,38 @@ def download_data(final_file_name="arxiv.json"):
     # Rename the file
     os.rename(join(data_dir, "arxiv-metadata-oai-snapshot.json"), join(data_dir, final_file_name))
 
+def process_data(processed_data_path = join(data_dir, "arxiv_processed.csv")):
+    
+    data_path = join(data_dir, "arxiv.json")
+    n_lines = sum(1 for line in open(data_path))
+    batch_size = 10000
+
+    # Check if the data has already been processed
+    if exists(processed_data_path):
+        print("Data has already been processed")
+        return
+
+    for df in tqdm(pd.read_json(data_path, lines=True, chunksize=batch_size), total=n_lines//batch_size, desc="Processing data", unit="batch"):
+        # Set datatype as string
+        df = df.astype(str)
+        # replace nan with empty string
+        df = df.replace("nan", "")
+
+        # All the features:
+        # ['id', 'submitter', 'authors', 'title', 'comments', 'journal-ref', 'doi',
+        #'report-no', 'categories', 'license', 'abstract', 'versions',
+        # 'update_date', 'authors_parsed']
+        #
+        # But we only some
+        df = df[['id', 'abstract', 'title', 'doi', 'categories', 'update_date', 'authors_parsed']]
+
+        # Remove rows with empty abstracts, titles, or ids
+        df = df[(df['abstract'] != "") & (df['title'] != "") & (df['id'] != "")]
+
+        # To csv
+        df.to_csv(processed_data_path, mode='a', header=True if not exists(processed_data_path) else False, index=False)
+
 
 if __name__ == "__main__":
-    download_data()
+    #download_data()
+    process_data()
